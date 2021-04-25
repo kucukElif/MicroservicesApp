e6a4e667791b7a179afc0c48b8531c8834d00bb8
elif.kucuk <e.kucuk@finmaks.com>
2021-04-25 20:19:25 +0300

    Catalog.Api protected by Identity.server

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using FreeCourse.IdentityServer.Dtos;
 using FreeCourse.IdentityServer.Models;
 using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,36 +9,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace FreeCourse.IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
 
-     public UserController(UserManager<ApplicationUser> userManager)
+        public UserController(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> SignUp(SignupDto signupDto) {
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
             var user = new ApplicationUser
             {
                 UserName = signupDto.UserName,
                 Email = signupDto.Email,
                 City = signupDto.City
             };
+
             var result = await _userManager.CreateAsync(user, signupDto.Password);
 
-            if (!result.Succeeded) {
+            if (!result.Succeeded)
+            {
                 return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
-    
             }
-            return NoContent();
 
-           
+            return NoContent();
         }
     }
 }
