062fba5e4281b69ea4b93162010e596bb26c49ca
elif.kucuk <e.kucuk@finmaks.com>
2021-04-25 20:56:06 +0300

    AllowOfflineAccess

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -48,6 +48,7 @@ namespace FreeCourse.IdentityServer
                 new Client{
                 ClientName="Asp.Net Core MVC",
                 ClientId = "WebMvcClientForUser",
+                AllowOfflineAccess=true,
                 ClientSecrets={ new Secret("secret".Sha256())},
                 AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
                 AllowedScopes={IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess},
